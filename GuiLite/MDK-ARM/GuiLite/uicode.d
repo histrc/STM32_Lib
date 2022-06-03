6a077e43b3c0a42ff74fba0202aec6d9bc7affdc
Qdashuai <Q_dashuai@163.com>
2022-06-03 23:44:52 +0800

    2022.6.3更新

---
+++ GuiLite/MDK-ARM/GuiLite/uicode.d
@@ -0,0 +1,7 @@
+guilite\uicode.o: ..\UIcode\UIcode.cpp
+guilite\uicode.o: ..\UIcode\GuiLite.h
+guilite\uicode.o: D:\MDK5\ARM\ARMCC\Bin\..\include\string.h
+guilite\uicode.o: D:\MDK5\ARM\ARMCC\Bin\..\include\stdio.h
+guilite\uicode.o: D:\MDK5\ARM\ARMCC\Bin\..\include\stdlib.h
+guilite\uicode.o: D:\MDK5\ARM\ARMCC\Bin\..\include\math.h
+guilite\uicode.o: ..\oled\oled.h
